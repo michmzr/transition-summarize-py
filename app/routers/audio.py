e4b851d6e23470410d5e16f2bfdfd717ef99acbe
Michał Mazur <michmzr@gmail.com>
2024-05-29 19:47:03 +0200

    Fixed tests

--- app/routers/audio.py
+++ app/routers/audio.py
@@ -2,7 +2,7 @@ import logging
 import os
 import tempfile
 
-from fastapi import UploadFile, APIRouter
+from fastapi import UploadFile, APIRouter, Response, status
 
 from transcribe.utils import transcribe
 
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 @router.post("/audio/transcribe")
-def audio_trans(uploadedFile: UploadFile):
+def audio_trans(uploadedFile: UploadFile, response: Response):
     """
     request accepts audio file in request multi-part and transcribes it to text
     :param uploadedFile:
@@ -21,13 +21,14 @@ def audio_trans(uploadedFile: UploadFile):
 
     # check if file is audio
     if not uploadedFile.content_type.startswith("audio"):
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return {"error": "Invalid file type. Only audio files are accepted"}
 
     # file ending with mp3/wav/omg
-    if not uploadedFile.filename.endswith(('flac', 'm4a', 'mp3', 'mp4', 'mpeg', 'mpga', 'oga', 'ogg', 'wav', 'webm')):
-        return {"error":
-                    "Invalid file type. Only 'flac', 'm4a', 'mp3', 'mp4', 'mpeg', 'mpga', 'oga', 'ogg', 'wav', "
-                    "'webm' files are accepted"}
+    exts = ('flac', 'm4a', 'mp3', 'mp4', 'mpeg', 'mpga', 'oga', 'ogg', 'wav', 'webm')
+    if not uploadedFile.filename.endswith(exts):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"error": f"Invalid file type. Only {exts} files are accepted"}
 
     suffix = os.path.splitext(uploadedFile.filename)[1]
     with tempfile.NamedTemporaryFile(suffix=suffix, delete=False) as temp:
@@ -40,4 +41,4 @@ def audio_trans(uploadedFile: UploadFile):
     os.unlink(temp.name)
 
     logging.info("Completed processing audio file. Returning transcription.")
-    return {"transcription": transcription}
+    return {"result": transcription}
--- testing/test_api.py
+++ testing/test_api.py
@@ -1,10 +1,26 @@
+import os
+import shutil
 import httpx
 import pytest
-
+from fastapi.testclient import TestClient
 from main import app
 
 BASE_URL = "http://127.0.0.1:8000/"
 SHORT_YT_VIDEO = "https://www.youtube.com/watch?v=WuciqTSbewY"
+
+client = TestClient(app)
+
+def test_audio_transcribe_valid_file():
+    with open('resources/audio_short.mp3', 'rb') as f:
+        response = client.post("/audio/transcribe", files={"uploadedFile": f})
+    assert response.status_code == 200
+    assert "result" in response.json()
+
+def test_audio_transcribe_invalid_file():
+    with open('resources/test_file.txt', 'rb') as f:
+        response = client.post("/audio/transcribe", files={"uploadedFile": f})
+    assert response.status_code == 400
+    assert "error" in response.json()
 
 @pytest.mark.asyncio
 async def test_youtube_transcribe():
@@ -21,3 +37,14 @@ async def test_youtube_summarize():
                                  json={"url":  SHORT_YT_VIDEO, "type": "TLDR"})
     assert response.status_code == 200
     assert "result" in response.json()
+    assert response.json()["result"] != ""
+
+
+def teardown_module(module):
+    """
+    This method is called once for each module after all tests in it are run.
+    We are using it to clean up the downloads directory after tests are run.
+    """
+    downloads_dir = './downloads'
+    if os.path.exists(downloads_dir):
+        shutil.rmtree(downloads_dir)

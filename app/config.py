42dce67e7ee15f2dcf9c1b2f51a829b5b6a1c267
Michał Mazur <michmzr@gmail.com>
2024-11-17 12:15:40 +0100

    changed import

--- app/config.py
+++ app/config.py
@@ -1,6 +1,11 @@
-from settings import get_settings
+import os
+from functools import lru_cache
+from app.settings import get_settings as _get_settings
 
+@lru_cache()
+def get_settings():
+    return _get_settings()
 
 def get_downloads_path():
     settings = get_settings()
-    return settings.data_dir 
+    return settings.data_dir
